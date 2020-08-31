880f34bb02a0c9b1053c68f102a46aece85d7977
Vladimir Smetankin <vs.smetankin@gmail.com>
2020-08-31 12:07:23 +0300

    scrap pages

---
+++ lp3/assets/js/themify.js
@@ -0,0 +1,1 @@
+;jQuery(document).ready(function($){$('.back-top a').click(function(){$('body,html').animate({scrollTop:0},800);return false;});$("#menu-icon").click(function(){$("#main-nav").fadeToggle();});if($('body').hasClass('touch')&&typeof jQuery.fn.themifyDropdown!='function'){Themify.LoadAsync(themify_vars.url+'/js/themify.dropdown.js',function(){$('#main-nav').themifyDropdown();});}});
